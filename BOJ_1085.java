ca51e3e140aef3209d50857983a86ddb5b515626
jootang2 <joohwan.song15b@gmail.com>
2022-08-03 00:16:36 +0900

    1085_JAVA_직사각형에서 탈출_입력방식 변경

--- BOJ_1085.java
+++ BOJ_1085.java
@@ -1,16 +1,20 @@
 package juhwan;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BOJ_1085 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int w = sc.nextInt();
-        int h = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine()," ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[4];
         arr[0] = x;
